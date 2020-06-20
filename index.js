15dc02b48b7fe5bbcc09245b860b451b37e7800c
climoralesg <38842608+climoralesg@users.noreply.github.com>
2020-06-20 02:27:55 -0400

    commit inicial

---
+++ index.js
@@ -0,0 +1,40 @@
+const express= require('express');
+const app=express();
+const path=require('path');
+const SockeoIO=require('socket.io');
+
+// se define el puerto
+app.set('port',4040);
+
+//console.log(path.join(__dirname,'public')); // Indica donde esta nuestro proyecto (nuestro viva la madre Rusia)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+
+//escucha por el puerto y despues ejecuta la funcion ()
+//iniciar el servidor
+const server = app.listen(app.get('port'),()=>{
+    console.log('servidor en puerto', app.get('port'));
+});
+
+//SockeoIO.listen(server);
+//prepara la escucja como socket
+const io=SockeoIO(server);
+
+//websocket
+//este se conecta con io();
+io.on('connection',(socket)=>{
+    //console.log("new connection", socket);
+    console.log("Nueva conexion", socket.id);
+    socket.on('chat:message',(data)=>{ //el emit del otro
+        io.sockets.emit('chat:message', data);
+        
+        //console.log(data);
+    });
+
+    socket.on('chat:typing',(data)=>{
+        socket.broadcast.emit('chat:typing',data) // emite a todos excepto a mi interfaz 
+        console.log(data);
+    })
+});
+
---
+++ public/socket.js
@@ -0,0 +1,38 @@
+console.log("codigo");
+//io(midominio:puerto);
+const socket=io();//esto funciona de forma local
+
+let message=document.getElementById("message");
+let userName=document.getElementById("userName");
+
+let btn=document.getElementById("send");
+let output=document.getElementById("output");
+let actions=document.getElementById("actions");
+
+btn.addEventListener('click',function(){
+    socket.emit('chat:message',{
+        userName:userName.value,
+        message:message.value
+    });
+    /*
+    console.log('Clickeado');
+    console.log(message);
+    console.log({
+        userName:userName.value,
+        message:message.value
+    })
+    */
+
+});
+message.addEventListener('keypress',function(){
+    socket.emit('chat:typing',userName.value);
+})
+
+socket.on('chat:message',function(data){
+    console.log("datos", data);
+    output.innerHTML += `<p> <strong>${data.userName}</strong>:${data.message}</p>`
+});
+
+socket.on('chat:typing',function(data){
+    actions.innerHTML=`<p><em>${data} is typing</em></p>`
+})
